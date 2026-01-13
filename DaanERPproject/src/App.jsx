81924d8a157e3df48bb55bc42dbd6e55624f50e0
anoopvr33 <anoopvranoop33@gmail.com>
2026-01-13 10:19:39 +0530

    trial

--- DaanERPproject/src/App.jsx
+++ DaanERPproject/src/App.jsx
@@ -6,7 +6,11 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <h1>Daan ERP Software</h1>
+    </div>
+  );
 }
 
 export default App;
