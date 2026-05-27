eea7760cdb3827b7993c9cac8607e8531480ce81
anoopvr33 <anoopvranoop33@gmail.com>
2026-05-27 13:49:31 +0530

    status color

--- DaanERPproject/src/components/bookingTable/index.jsx
+++ DaanERPproject/src/components/bookingTable/index.jsx
@@ -183,7 +183,9 @@ const BookingTable = ({ SortedDays }) => {
                           ? "red"
                           : i.tag_status === "Confirmed"
                             ? "rgb(74, 194, 74)"
-                            : "",
+                            : i.tag_status === "Checked Out"
+                              ? "rgb(24, 124, 255)"
+                              : "",
                     }}
                   >
                     {i.tag_status}
