397d85b874cf737a7a806e88f9f777b4725c3305
anoopvr33 <anoopvranoop33@gmail.com>
2026-06-25 10:50:17 +0530

    meta vit env

--- DaanERPproject/src/utils/axios.js
+++ DaanERPproject/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "https://admin.daanregency.com/",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
